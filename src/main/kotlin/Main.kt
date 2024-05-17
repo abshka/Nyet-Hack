da7a5f55e2124c1ac21df6a341c41a4d8cfd5ac1
AB <markelofaleksei@gmail.com>
2024-05-17 18:08:07 +0300

    Задание "Списки"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
-fun main() {
-    narrate("A hero enters the town of Kronstadt. What is their name?") { message -> "\u001b[33;1m$message\u001b[0m"}
-    val heroName = readLine()
-        require(heroName != null && heroName.isNotEmpty()) { "The hero must have a name." }
+var heroName: String = ""
 
-    changeNarratorMood()
+fun main() {
+
+    heroName = promptHeroName()
+    // changeNarratorMood()
     narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+    visitTavern()
 }
 
 private fun createTitle(name: String): String {
@@ -27,4 +28,17 @@ private fun createTitle(name: String): String {
         name.zipWithNext().any { it.first == it.second } -> "The Unconquerable"
         else -> "The Renowned Hero"
     }
+}
+
+private fun promptHeroName(): String {
+    narrate("A hero enters the town of Kronstadt. What is their name?") {
+        message -> "\u001b[33;1m$message\u001b[0m"
+    }
+    /*val input = readline()
+    require(input != null && input.isNotEmpty()) {
+        "The hero must have a name."
+        }
+     return input*/
+    println("Madrigal")
+    return "Madrigal"
 }
